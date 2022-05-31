4d523a8cff575893ad899c5579d2f6b0d31d47c8
Takashi Doyama <63372896+donchan91@users.noreply.github.com>
2022-05-31 23:38:10 +0200

    Add files via upload
    Added Ascii Art (pardon my sense of humour), option for UDP scan. 
    
    To do: Create a list of ports using grep-fu etc. and get the tool to run specific tools e.g. nikto for port 80

--- recceboi.py
+++ recceboi.py
@@ -4,24 +4,68 @@ import os,sys
 #Testing out command
 #os.system("echo Hello world brought to you by command line.")
 
+def strike(text):
+    return ''.join([u'\u0336{}'.format(c) for c in text])
+
+
+
+print(
+
+"""
+
+
+=================================================================
+
+ ===========
+|           |
+|           |                            
+|===========                            
+|\         ---    ----   ----    ---    
+| \       |   |  |      |       |   |   
+|  \      ----   |      |       ----    
+|   \     |      |      |       |       
+|    \     ----   ----   ----    ----    
+
+"""
+
+)
+print("Because no-one ever spells reconesance properly" )
+print(strike("test"))
+print("=================================================================")
+
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} <IP> ")
     sys.exit()
 
 ip=sys.argv[1]
 
+start = True 
+
 #NMAP scan
 
 def nmap(ip):
-    os.system("mkdir nmap")
-    print("Running NMAP stealth scan")
-    os.system(f"sudo nmap -sS -p- -Pn -vvv --script default,vuln -sV -oA nmap/tcp {ip}")
-    print("Do you also want to do UDP scanning? Y/N")
-    response = input("Do you also want to do UDP scanning (top 1024)? Y/N  ").lower()
-    if response == "y":
-        os.system(f"sudo nmap -sU -vvv --top-ports=1024 -oA nmap/udp --script default,vuln -Pn {ip}"  )
-print(f"Running scripted nmap on {ip}")
+    global start
+    os.system("mkdir scans")
+    os.system("mkdir scans/nmap")
+    os.system(f"mkdir scans/nmap/{ip}")
+    print(f"Running NMAP stealth scan on {ip} ")
+    os.system(f"sudo nmap -sS -p- -Pn -vvv --script default,vuln -sV -oA scans/nmap/{ip}/tcp {ip}")
+    print(f"Creating a textfile with a list of open TCP Ports")
+    os.system(f"cat scans/nmap/{ip}/tcp.nmap | grep tcp | grep open | cut -f 1 -d '/' >> scans/opentcpports.txt ")
+    while start == True:
+        response = input("Do you also want to do UDP scanning (top 1024)? Y/N  ").lower()
+        if response == "y":
+            print(f"Running UDP scan on {ip} ")
+            os.system(f"sudo nmap -sU -vvv --top-ports=1024 -oA scans/nmap/{ip}/udp --script default,vuln -Pn {ip}"  )
+            sys.exit()
+        elif response == "n":
+            start = False
+            sys.exit()
+        else:
+            print("Please enter a valid input Y or N")
 
-print(str(len(sys.argv)))
+#Nikto scan
+def nikto(ip):
+    os.system("mkdir scans/nikto")
 
 nmap(ip)
